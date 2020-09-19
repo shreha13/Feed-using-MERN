99575f4e9e44f83c7314af24f4719b111cb438a4
shreha13 <shreyadidwania95@gmail.com>
2020-09-19 11:18:00 +0530

    graphql

--- FeedProject-Node/app.js
+++ FeedProject-Node/app.js
@@ -4,9 +4,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 const multer = require("multer");
 const { uuid } = require("uuidv4");
+const { graphqlHTTP } = require("express-graphql");
 
 const feedRoutes = require("./routes/feed");
 const authRoutes = require("./routes/auth");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/is-auth");
 
 const app = express();
 
@@ -49,11 +53,52 @@ app.use((req, res, next) => {
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
+app.use(auth);
+
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    const error = new Error("Not authenticated");
+    error.statusCode = 401;
+    throw error;
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file" });
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored", filePath: req.file.path });
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An unexpected error occurred";
+      const status = err.originalError.code || 500;
+      return {
+        message: message,
+        data: data,
+        status: status,
+      };
+    },
+  })
+);
+
+// app.use("/feed", feedRoutes);
+// app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
@@ -66,10 +111,10 @@ mongoose
     "mongodb+srv://shreha:IAtsWXsiAfKIvkSX@cluster0.u5mts.mongodb.net/messages?retryWrites=true&w=majority"
   )
   .then(() => {
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('Client connected!')
-    })
+    app.listen(8080);
+    // const io = require("./socket").init(server);
+    // io.on("connection", (socket) => {
+    //   console.log("Client connected!");
+    // });
   })
   .catch((err) => console.log(err));
--- FeedProject-Node/middleware/is-auth.js
+++ FeedProject-Node/middleware/is-auth.js
@@ -3,25 +3,30 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    const error = new Error("Not authenticated");
-    error.statusCode = 401;
-    throw error;
+    // const error = new Error("Not authenticated");
+    // error.statusCode = 401;
+    // throw error;
+    req.isAuth = false;
+    return next();
   }
   let decodedToken;
   const token = authHeader.split(" ")[1];
   try {
     decodedToken = jwt.verify(token, "secretkey");
   } catch (err) {
-    const error = new Error("Not authenticated");
-    error.statusCode = 401;
-    throw error;
+    // const error = new Error("Not authenticated");
+    // error.statusCode = 401;
+    // throw error;
+    req.isAuth = false;
   }
 
   if (!decodedToken) {
-    const error = new Error("Not authenticated");
-    error.statusCode = 401;
-    throw error;
+    // const error = new Error("Not authenticated");
+    // error.statusCode = 401;
+    // throw error;
+    req.isAuth = false;
   }
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 };
